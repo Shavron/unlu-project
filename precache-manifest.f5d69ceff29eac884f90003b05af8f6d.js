2a2838b94e632b19c3f3e4241b9d2562c97e1b4e
Shavron <ashu.9x@gmail.com>
2020-07-26 13:19:26 +0530

    Updates

---
+++ precache-manifest.f5d69ceff29eac884f90003b05af8f6d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b97ccc8283711766a1036c922155ab",
+    "url": "/unlu-project/index.html"
+  },
+  {
+    "revision": "93d1b69816b680f0d80c",
+    "url": "/unlu-project/static/css/2.cf4bd975.chunk.css"
+  },
+  {
+    "revision": "b2220d1fd9d12f1ae114",
+    "url": "/unlu-project/static/css/main.d1e05458.chunk.css"
+  },
+  {
+    "revision": "93d1b69816b680f0d80c",
+    "url": "/unlu-project/static/js/2.e2e05bb6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/unlu-project/static/js/2.e2e05bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2220d1fd9d12f1ae114",
+    "url": "/unlu-project/static/js/main.0435a920.chunk.js"
+  },
+  {
+    "revision": "79074595699b8fb1c837",
+    "url": "/unlu-project/static/js/runtime-main.6e07cb1e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/unlu-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/unlu-project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/unlu-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/unlu-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/unlu-project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
