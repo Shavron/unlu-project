63e53f087925ebcaa32f4ec45e7d597bc0ab8695
Shavron <ashu.9x@gmail.com>
2020-07-26 13:47:03 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
 import Feed from "./components/Feed";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      hi
       <BrowserRouter>
         {/* <Route
           exact
