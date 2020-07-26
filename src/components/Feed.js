66f008c1ec3a6f4c9c8316e36e42d4547abaad93
Shavron <ashu.9x@gmail.com>
2020-07-26 16:40:36 +0530

    https remove

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -15,9 +15,9 @@ export default function Feed() {
   const [error, setError] = useState({ error: false, msg: "" });
 
   const ApiArray = [
-    "//www.mocky.io/v2/59b3f0b0100000e30b236b7e",
-    "//www.mocky.io/v2/59ac28a9100000ce0bf9c236",
-    "//www.mocky.io/v2/59ac293b100000d60bf9c239"
+    "http://www.mocky.io/v2/59b3f0b0100000e30b236b7e",
+    "http://www.mocky.io/v2/59ac28a9100000ce0bf9c236",
+    "http://www.mocky.io/v2/59ac293b100000d60bf9c239"
   ];
 
   useEffect(() => {
